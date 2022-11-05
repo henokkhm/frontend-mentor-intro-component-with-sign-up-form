eb40eb79fe914cf24228adfbc4fd4c857163812a
Henok Hailemariam <henokhm2@gmail.com>
2022-11-05 23:34:41 +0300

    Add tailwind classes for mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,10 @@ module.exports = {
     extend: {
       colors: {
         "custom-red": "hsl(0, 100%, 74%)",
-        "custom-green": "hsl(154, 59%, 51%",
-        "custom-blue": "hsl(248, 32%, 49%",
+        "custom-green": "hsl(154, 59%, 51%)",
+        "custom-blue": "hsl(248, 32%, 49%)",
         "custom-dark-blue": "hsl(249, 10%, 26%)",
-        "custom-grayish-blue": "hsl(246, 25%, 77%",
+        "custom-grayish-blue": "hsl(246, 25%, 77%)",
       },
     },
   },
