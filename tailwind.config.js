b38cc1caf645d0e36583d60be033ca00841789b5
Henok Hailemariam <henokhm2@gmail.com>
2022-11-05 19:55:41 +0300

    Add style guide fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      "custom-red": "hsl(0, 100%, 74%)",
+      "custom-green": "hsl(154, 59%, 51%",
+      "custom-blue": "hsl(248, 32%, 49%",
+      "custom-dark-blue": "hsl(249, 10%, 26%)",
+      "custom-grayish-blue": "hsl(246, 25%, 77%",
+    },
+    fontFamily: {
+      poppins: "Poppins",
+    },
     extend: {},
   },
   plugins: [],
